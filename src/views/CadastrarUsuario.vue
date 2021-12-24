825e430521c0b7e6895c1e1da5037b3df3a6c87e
Stella <stellamaia97@hotmail.com>
2021-12-24 16:20:37 -0300

    Grid e form

--- src/views/CadastrarUsuario.vue
+++ src/views/CadastrarUsuario.vue
@@ -1,13 +1,70 @@
 <template>
-  <div id="app">
-    <input type="text" v-model="usuarioName" @keyup.enter="addUsuario" />
-    <div v-for="ultimoUsuario of ultimosUsuarios" :key="ultimoUsuario.id">
-      
-      {{ ultimoUsuario.name }}
-      <hr />
-      
-    </div>
-  </div>
+  <b-container>
+    <b-row>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-1" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.name"
+            type="text"
+            placeholder="Nome"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.age"
+            type="number"
+            placeholder="Idade"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-3" label="Genêro:" label-for="input-3">
+          <b-form-select
+            id="input-3"
+            v-model="form.gender"
+            :options="genderList"
+            required
+          ></b-form-select>
+        </b-form-group>
+
+        <b-form-group id="input-group-3" label="Cidade:" label-for="input-3">
+          <b-form-select
+            id="input-3"
+            v-model="form.city"
+            :options="cidadeList"
+            required
+          ></b-form-select>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-row>
+    <b-row>
+      <b-col
+        class="mt-3"
+        sm="12"
+        lg="6"
+        v-for="usuario of ultimosUsuarios"
+        :key="usuario.id"
+      >
+        <b-card border-variant="info" align="center" :header="usuario.name">
+          <b-card-text class="card-usuario">
+            <span> {{ usuario.gender }}</span
+            ><br />
+            <hr />
+            <span> {{ usuario.age }} anos </span><br />
+            <hr />
+            <span> {{ usuario.city }} </span> <br />
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -22,6 +79,15 @@ export default {
       usuarios: [],
       usuarioName: "",
       ultimosUsuarios: [],
+      form: {
+        name: "",
+        gender: "",
+        age: null,
+        city: "",
+      },
+      cidadeList: ["Uberlândia", "São Paulo", "Santa Catarina", "Brasília"],
+      genderList: ["Feminino", "Masculino"],
+      show: true,
     };
   },
 
@@ -32,11 +98,25 @@ export default {
   },
 
   methods: {
-    addUsuario() {
-      axios.post(baseURL, { name: this.usuarioName }).then((res) => {
+    onSubmit(event) {
+      event.preventDefault();
+
+      axios.post(baseURL, this.form).then((res) => {
         this.usuarios = [...this.usuarios, res.data];
         this.ultimosUsuarios = [...this.ultimosUsuarios, res.data];
         this.usuarioName = "";
+      });
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.form.gender = "";
+      this.form.name = "";
+      this.form.age = null;
+      this.form.city = "";
+
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
       });
     },
   },
--- src/views/ListarUsuario.vue
+++ src/views/ListarUsuario.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
-    <input class="search" v-model="searchTerm" type="text" />
-    <b-row >
-     
-        <b-col sm="12" lg="6">
-          <b-card  v-for="usuario of usuariosFiltrados" :key="usuario.id" border-variant="info" align="center" :header="usuario.name">
-            <b-card-text class="card-usuario">
-              <span> {{ usuario.gender }}</span><br />
-              <hr />
-              <span> {{ usuario.age }} anos </span><br />
-              <hr />
-              <span> {{ usuario.city }} </span> <br />
-            </b-card-text>
-          </b-card>
-        </b-col>
- 
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form-input class="search" v-model="searchTerm" type="text" />
+      </b-col>
     </b-row>
-</div>
+    <b-row>
+      <b-col
+        class="mt-3"
+        sm="12"
+        lg="6"
+        v-for="usuario of usuariosFiltrados"
+        :key="usuario.id"
+      >
+        <b-card border-variant="info" align="center" :header="usuario.name">
+          <b-card-text class="card-usuario">
+            <span> {{ usuario.gender }}</span
+            ><br />
+            <hr />
+            <span> {{ usuario.age }} anos </span><br />
+            <hr />
+            <span> {{ usuario.city }} </span> <br />
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
